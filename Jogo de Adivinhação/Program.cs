29a42a04f1dc99cd7b166dc3eaca7b4ac5f19d5b
Marco Antonio <marcotonho38@gmail.com>
2023-07-20 20:52:31 -0300

    FEAT : add jogo

--- Jogo de Adivinhação/Program.cs
+++ Jogo de Adivinhação/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Random aleatorio = new Random();
+int numeroSecreto = aleatorio.Next(1, 101);
+
+do
+{
+    Console.Write("Digite um número entre 1 e 100: ");
+    int chute = int.Parse(Console.ReadLine());
+
+    if (chute == numeroSecreto)
+    {
+        Console.WriteLine("Parabéns! Você acertou o número.");
+        break;
+    }
+    else if (chute < numeroSecreto)
+    {
+        Console.WriteLine("O número é maior.");
+    }
+    else
+    {
+        Console.WriteLine("O número é menor.");
+    }
+
+} while (true);
